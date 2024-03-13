a6c598ef22905989c7e7ce115f50bcdc583473fd
Yura192c <fpm.mironchiYU@bsu.by>
2024-03-13 17:03:40 +0300

    Rename QueryBuilder class to SqlQueryBuilder

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from arg_parser import ArgumentParser
 from database.db_manager import DatabaseManager
-from database.query_builder import QueryBuilder
+from database.sql_query_builder import SqlQueryBuilder
 from database.data_processor import DataProcessor
 from database.output_manager import OutputManager
-
 
 if __name__ == "__main__":
     args = ArgumentParser.parse_args()
@@ -18,21 +17,19 @@ if __name__ == "__main__":
             query = None
             match args.fetch:
                 case 'room_list':
-                    query = QueryBuilder.build_room_list_query()
+                    query = SqlQueryBuilder.build_room_list_query()
                 case 'rooms_avg_lowest_age':
-                    query = QueryBuilder.build_rooms_avg_lowest_age_query()
+                    query = SqlQueryBuilder.build_rooms_avg_lowest_age_query()
                 case 'rooms_avg_biggest_age':
-                    query = QueryBuilder.build_rooms_avg_biggest_age_query()
+                    query = SqlQueryBuilder.build_rooms_avg_biggest_age_query()
                 case 'rooms_opposite_sexes':
-                    query = QueryBuilder.build_rooms_opposite_sexes_query()
+                    query = SqlQueryBuilder.build_rooms_opposite_sexes_query()
                 case _:
                     query = ''
-
+            room_list = []
             if query:
                 rows = db_manager.execute_query(query)
                 if rows:
-                    if args.fetch == 'room_list':
-                        room_list = DataProcessor.process_room_list_data(rows)
                     match args.fetch:
                         case 'room_list':
                             room_list = DataProcessor.process_room_list_data(
@@ -46,9 +43,6 @@ if __name__ == "__main__":
                         case 'rooms_opposite_sexes':
                             room_list = DataProcessor.process_rooms_opposite_sexes_data(
                                 rows)
-                        case _:
-                            room_list = []
-
             match args.format:
                 case 'json':
                     OutputManager.save_to_json(room_list, args.output_filename)
